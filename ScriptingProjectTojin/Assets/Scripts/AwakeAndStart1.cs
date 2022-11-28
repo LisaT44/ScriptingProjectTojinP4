6badea451cbf3b43fe7b8309617f8d4e1e64ae3b
ElisaTojin <elisactojin@gmail.com>
2022-11-28 11:45:38 -0800

    I did the awake and start script
    I finished the awake and start script and attached it to a cube.

--- ScriptingProjectTojin/Assets/Scripts/AwakeAndStart1.cs
+++ ScriptingProjectTojin/Assets/Scripts/AwakeAndStart1.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class AwakeAndStart1 : MonoBehaviour
 {
+
+    void Awake ()
+    {
+        Debug.Log("Awake called.");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start called.");
     }
 
     // Update is called once per frame
