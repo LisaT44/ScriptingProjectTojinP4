86e8536546a9a3e377eb2a982c4aea4c43023e33
ElisaTojin <elisactojin@gmail.com>
2023-01-31 11:07:37 -0800

    Linear Interpolation
    I added a script to my prjecty

---
+++ ScriptingProjectTojin/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+  * 
+   //In this case, result = 4
+    float result = Mathf.Lerp (3f, 5f, 0.5f);
+    //the Math.Lerp function takesd 3 float parameters: one representing the value to intepolate from; another representing the value to interpolate to and a final float representing how far to interpolate
+  
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+
+    //Here result = (4, 5, 6)
+    Vector3 result = Vector3.Lerp (from, to, 0.75f);
+    //Vector3.Lerp finds some percentage of the way between two given vectors
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intesity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+        //this changes the light intensity over time staring at 0 and frame by frame it goes to 8
+
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+        //in this the light intensity would change every second instead of every frame
+    }
+*/
+}
